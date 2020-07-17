f5fda71e3a40822456322fca5f8cac330fea6d49
Jacob Valero <54868391+Orelavant@users.noreply.github.com>
2020-07-17 12:13:37 -0600

    Removed current graphs as they did not account for presence of cylinder/avatar

--- EDAFiles/EDA.py
+++ EDAFiles/EDA.py
@@ -61,15 +61,15 @@ cylinderClean = cleanData(cylinderRaw, "cylinder")
 
 # TODO: Compare ball rotation & response times across the two studies
 # Barplots of response time over ball rotation
-# avatarBarPlot = sns.barplot(x=avatarClean['Ball_Rotation'], y=avatarClean['Response_Time']).set_title('Avatar: Ball Rotation & Response Time')
+# sns.catplot(x=avatarClean['Ball_Rotation'], y=avatarClean['Response_Time'], hue=avatarClean["Chair_Type"]).set_title('Avatar: Ball Rotation & Response Time')
 # plt.show()
-# cylinderBarPlot = sns.barplot(x=cylinderClean['Ball_Rotation'], y=cylinderClean['Response_Time']).set_title('Cylinder: Ball Rotation & Response Time')
+# sns.catplot(x=cylinderClean['Ball_Rotation'], y=cylinderClean['Response_Time'], hue=cylinderClean["Chair_Type"]).set_title('Cylinder: Ball Rotation & Response Time')
 # plt.show()
 
 # Barplots of response time over ball distance
-# avatarBarPlot = sns.barplot(x=avatarClean['Ball_Distance'], y=avatarClean['Response_Time']).set_title('Avatar: Ball Distance & Response Time')
+# sns.barplot(x=avatarClean['Ball_Distance'], y=avatarClean['Response_Time']).set_title('Avatar: Ball Distance & Response Time')
 # plt.show()
-# cylinderBarPlot = sns.barplot(x=cylinderClean['Ball_Distance'], y=cylinderClean['Response_Time']).set_title('Cylinder: Ball Distance & Response Time')
+# sns.barplot(x=cylinderClean['Ball_Distance'], y=cylinderClean['Response_Time']).set_title('Cylinder: Ball Distance & Response Time')
 # plt.show()
 
 
